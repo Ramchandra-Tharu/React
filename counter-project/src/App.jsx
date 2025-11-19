ae9defb0074cfd1aac6896a84eb6cb12b316362c
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-11-20 01:21:34 +0530

    new project is created

---
+++ counter-project/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const addValue = () =>{
+    setCounter (counter + 1)
+  }
+
+
+  return(
+    <div>
+      <h1>Hey Sirzan </h1>
+      <h3>Welcome to increase button </h3>
+      <h4>counter:{counter}</h4>
+      <button
+      onClick = {addValue}
+      > Counter ++ </button>
+      <br />
+      <button> Counter --</button>
+    </div>
+
+  )
+ 
+}
+
+
+export default App
