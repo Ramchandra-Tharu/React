42e7b1e82ea370b920499c50b3090fb2a16d968e
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-11-19 22:56:17 +0530

    new project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Hello from "./Hello";
+import Counter from "./Counter";
+function App(){
+  return(
+    <div>
+      <h4>hello sirzan welcome to react</h4>
+      <Hello />
+      <Counter />
+    </div>
+
+   
+    
+  );
+}
+export default App;
---
+++ my-project/src/Hello.jsx
@@ -0,0 +1,4 @@
+function Hello(){
+    return <h2>this is my project</h2>;
+}
+export default Hello;
